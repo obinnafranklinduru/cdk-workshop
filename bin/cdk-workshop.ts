0544e0ff500a6bbbdb8a0825439571da20529e70
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-02-24 15:33:22 -0800

    initial commit

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import * as cdk from "aws-cdk-lib";
+import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new CdkWorkshopStack(app, "CdkWorkshopStack", {
+  env: {
+    account: "940593862958",
+    region: "us-east-1",
+  },
+});
